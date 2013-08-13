ccfe348f1a31c40d1c17541c67d1fed07514be01
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-13 15:33:07 -0400

    hashes and tile changes

--- kublai.js
+++ kublai.js
@@ -18,25 +18,88 @@
 		res.header('X-Powered-By', 'Kublai');
 		return next();
 	});
-	
-	
 	mbtiles.registerProtocols(tilelive);
 	
-	const layers = new Map();
-	
-	tilelive.list(tilePath, function(e, list) {
-		if (!e) {
-			const keys = Object.keys(list);
-			keys.forEach(function(key) {
-				tilelive.load(list[key], function(err, tileSource) {
-					if (!err) {
-						layers.set(key,tileSource);
-					}
-				});
-			});
+	function arrayEquality(a,b){
+		if(!(Array.isArray(a)&&Array.isArray(a))){
+			return false;
+		}else if(a.length!==b.length){
+			return false;
+		}else{
+			a.sort();
+			b.sort();
+			for(let i = 0,len=a.length;i<len;i++){
+				if(a[i]!==b[i]){
+					return false;
+				}
+			}
+			return true;
 		}
+	}
+	function parseInfo(resp,key){
+		resp.tilejson = '2.1.0';
+		if(resp.scheme){
+			delete resp.scheme;
+		}
+		resp.tiles = [];
+		let subDomains = ['a', 'b', 'c', 'd'];
+		if(domain.indexOf('*')===-1){
+			subDomains = [''];
+		}
+		for (let i = 0, len = subDomains.length; i < len; i++) {
+			resp.tiles.push("http://" + templateDomain(subDomains[i])+"/" + key + "/{z}/{x}/{y}.png");
+		}
+		resp.leaflet = "http://"+templateDomain("{s}")+"/" + key + "/{z}/{x}/{y}.png";
+		if(resp.template){
+			resp.grids=[];
+			for (let i = 0, len = subDomains.length; i < len; i++) {
+				resp.grids.push("http://" + templateDomain(subDomains[i])+"/" + key + "/{z}/{x}/{y}.grid.json");
+			}
+			resp.leafletGrid = "http://"+templateDomain("{s}")+"/" + key + "/{z}/{x}/{y}.grid.json";
+		}
+		return resp;
+	}
+	const layers = new Map();
+	let layerList = [];
+	let layerInfo = {};
+	function getLayers (){
+		tilelive.list(tilePath, function(e, list) {
+			if (!e) {
+				if(layerList.length){
+					layerList.forEach(function(layer){
+						layers.delete(layer);
+					})
+				}
+				layerList = Object.keys(list);
+				layerInfo = {};
+				layerList.forEach(function(key) {
+					tilelive.load(list[key], function(err, tileSource) {
+						if (!err) {
+							layers.set(key,tileSource);
+							tileSource.getInfo(function(err, resp) {
+								if (!err) {
+									layerInfo[key]=parseInfo(resp,key);
+								}
+							});
+						}
+					});
+				});
+			}
+		});
+	}
+	getLayers();
+	fs.watch(tilePath,getLayers);
+	kublai.get('/', function(req, res, next) {
+		res.jsonp(layerList.map(function(layer){
+			const out = {};
+			out.preview=layer+'/preview';
+			out.id=layer;
+			if(layer in layerInfo){
+				out.info = layerInfo[layer];
+			}
+			return out;
+		}));
 	});
-	
 	kublai.get('/:layer/:z/:x/:y.png', function(req, res, next) {
 		if (layers.has(req.params.layer)) {
 			layers.get(req.params.layer).getTile(req.params.z, req.params.x, req.params.y, function(err, tile) {
@@ -97,27 +160,7 @@
 						err: err
 					});
 				} else {
-					resp.tilejson = '2.1.0';
-					if(resp.scheme){
-						delete resp.scheme;
-					}
-					resp.tiles = [];
-					let subDomains = ['a', 'b', 'c', 'd'];
-					if(domain.indexOf('*')===-1){
-						subDomains = [''];
-					}
-					for (let i = 0, len = subDomains.length; i < len; i++) {
-						resp.tiles.push("http://" + templateDomain(subDomains[i])+"/" + req.params.layer + "/{z}/{x}/{y}.png");
-					}
-					resp.leaflet = "http://"+templateDomain("{s}")+"/" + req.params.layer + "/{z}/{x}/{y}.png";
-					if(resp.template){
-						resp.grids=[];
-						for (let i = 0, len = subDomains.length; i < len; i++) {
-							resp.grids.push("http://" + templateDomain(subDomains[i])+"/" + req.params.layer + "/{z}/{x}/{y}.grid.json");
-						}
-						resp.leafletGrid = "http://"+templateDomain("{s}")+"/" + req.params.layer + "/{z}/{x}/{y}.grid.json";
-					}
-					res.jsonp(resp);
+					res.jsonp(parseInfo(resp,req.params.layer));
 				}
 			});
 		} else {
