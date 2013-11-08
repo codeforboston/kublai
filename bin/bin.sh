117bcfa49d081f32183ce49966afee6af1e2edba
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-08 10:35:42 -0500

    better bin

--- bin/bin.sh
+++ bin/bin.sh
@@ -1,9 +1,5 @@
-#!/usr/bin/env bash
-
-if [ -L $0 ] ; then
-    DIR=$(dirname $(readlink -f $0)) ;
-else
-    DIR=$(dirname $0) ;
-fi ;
-
-node --harmony $DIR/kublai $@
+#!/bin/sh
+# from https://github.com/olov/defs/blob/master/defs-harmony
+rdlkf() { [ -L "$1" ] && (local lk="$(readlink "$1")"; local d="$(dirname "$1")"; cd "$d"; local l="$(rdlkf "$lk")"; ([[ "$l" = /* ]] && echo "$l" || echo "$d/$l")) || echo "$1"; }
+DIR="$(dirname "$(rdlkf "$0")")"
+/usr/bin/env node --harmony "$DIR/kublai" "$@"
